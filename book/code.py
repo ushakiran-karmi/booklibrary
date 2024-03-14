c80f2aa37d59039e791c51648d024f2ae38bf9d0
collegemanagement <ushakirankarmankar@gmail.com>
2024-03-14 13:36:58 +0530

    added list

--- book/code.py
+++ book/code.py
@@ -6,24 +6,24 @@ class Books:
         self.isbn=""
         
     def inputBookDetails(self):
-        self.name=input("enter the book name:")
-        self.author=input("enter the authoe name:")
-        self.price=float(input("enter the book price:"))
-        self.isbn=input("enter book ISBN number:")
-        print(f"{self.name} book added successfully:")
+        self.name=input("Enter the book name:")
+        self.author=input("Enter the author name:")
+        self.price=float(input("Enter the book price:"))
+        self.isbn=input("Enter book ISBN number:")
+        print(f"{self.name} Book added successfully:")
         print("-"*20)
         
     def booksdisplay(self):
-        print(f'book name : {self.name}')
-        print(f'author name : {self.author}')
-        print(f'book price : {self.price}')
-        print(f'book ISBN NO : {self.isbn}')
+        print(f'Book name : {self.name}')
+        print(f'Author name : {self.author}')
+        print(f'Book price : {self.price}')
+        print(f'Book ISBN NO : {self.isbn}')
         print("-"*20)
         
 my_booklist=[]
 
 while True:  
-    choice=int(input("enter your choice \n1. Add book \n2. display book \n3.stop\n"))
+    choice=int(input("Enter your choice \n1. Add book \n2. Display book \n3.Stop\n"))
     
     if choice==1:
         b=Books()
@@ -31,14 +31,20 @@ while True:
         my_booklist.append(b)   
         
     elif choice==2:
-        for i in my_booklist:
-            i.booksdisplay()
+        if len(my_booklist)==0:
+            print("No books are available")
+        else:
+            for i in my_booklist:
+                i.booksdisplay()
+            
+            
             
     elif choice==3:
         print("Thanks for using application")
         break 
     
     else:
-        print("enter a valid option")       
+        print("Enter a valid option")       
             
+print(my_booklist)            
             
